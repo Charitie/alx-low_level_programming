7faa5c23996fa4b3487f04d2f90ebe65e426d508
charitie <charitytarus21@gmail.com>
2021-02-09 13:11:50 +0000

    Modify task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
 *main - Prints String
 *Description: A program to print Holberton
@@ -7,7 +7,13 @@
 
 int  main(void)
 {
-printf("Holberton \n");
+char c;
+char s[] = "Holberton \n";
+int i;
+
+for (i = 0; i < 9; i++) 
+c=s[i];
+_putchar(c);
 
 return (0);
 }
