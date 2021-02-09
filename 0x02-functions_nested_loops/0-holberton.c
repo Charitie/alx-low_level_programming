b07fb1ad05118babb07d1deba402de82015b7434
charitie <charitytarus21@gmail.com>
2021-02-09 11:59:35 +0000

    Added task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - Prints String
+*Description: A program to print Holberton
+*Return: 0
+*/
+
+int  main(void)
+{
+printf("Holberton \n");
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x02-functions_nested_loops
+
+**0-holberton.c -** A  program that prints Holberton, followed by a new line.
