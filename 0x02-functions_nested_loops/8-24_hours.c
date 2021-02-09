aec8bc54c31e3d8b9969290c0f399ad6614d42ec
charitie <charitytarus21@gmail.com>
2021-02-09 16:53:50 +0000

    Modify task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,10 +10,9 @@ int h, m;
 
 for (h = 0; h < 24; h++)
 for (m = 0; m < 60; m++)
-_putchar('0' + (h / 10));
-_putchar('0' + (h % 10));
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar('0' + (m / 10));
-_putchar('0' + (m % 10));
-_putchar('\n');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
 }
