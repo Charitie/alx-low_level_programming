a8bf94bca16b2b9bdf6e7f6cab549ce80c8e3add
charitie <charitytarus21@gmail.com>
2021-02-11 16:37:11 +0000

    Modify task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
 *
 *Return: Nothing
 */
-
 void print_numbers(void)
 {
-char c;
+	char c;
 
-for (c = '0'; c <= '9'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
