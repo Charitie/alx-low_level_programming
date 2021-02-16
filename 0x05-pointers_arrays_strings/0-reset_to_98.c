6e89cc92ec86fb88e319a6e5e8ffe7d26908a9c5
charitie <charitytarus21@gmail.com>
2021-02-16 15:11:04 +0000

    add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+*reset_to_98 - pointer to an int as parameter
+* and updates the value it points to
+*@n: pointer
+*
+*Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
