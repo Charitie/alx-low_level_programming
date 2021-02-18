11ddcefe0875981bb6accfdd7384cdf71d4f105b
charitie <charitytarus21@gmail.com>
2021-02-18 19:18:33 +0000

    Modify task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,18 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 
-for (i = 0; dest[i] != '\0'; i++)
+i = 0;
+while (dest[i] != '\0')
 {
-continue;
-}
-
-for (i = 0; src[j] != '\0'; j++)
-{
-dest[i] += src[j];
 i++;
 }
 
-dest += '\0';
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + j] = src[j];
+}
+
+dest[i + j] = '\0';
 
 return (dest);
 }
