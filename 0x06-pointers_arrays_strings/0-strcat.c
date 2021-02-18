2caf7c448e6fe7b631899f471e6139f4dc3c2e39
charitie <charitytarus21@gmail.com>
2021-02-18 18:33:03 +0000

    Add task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest: string
+*@src: string
+*Return: pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+continue;
+}
+
+for (i = 0; src[j] != '\0'; j++)
+{
+dest[i] += src[j];
+i++;
+}
+
+dest += '\0';
+
+return (dest);
+}
