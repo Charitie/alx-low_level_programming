74d072dedb7c14bdb339c9d17e1df57ca3b7aa4b
charitie <charitytarus21@gmail.com>
2021-02-22 17:40:13 +0000

    Modify task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,14 +15,14 @@ sum = 0;
 
 for (i = 0; i < size; i++)
 {
-sum += *(a + i *size + i);
+sum += *(a + i * size + i);
 }
 printf("%i, ", sum);
 sum = 0;
 
 for (j = 0; j < size; j++)
 {
-sum += *(a + size *(j + 1) - (j + 1));
+sum += *(a + size * (j + 1) - (j + 1));
 }
 printf("%i\n", sum);
 }
