037d4528a8ea6289787950e9664104f147f97b6f
charitie <charitytarus21@gmail.com>
2021-02-28 20:50:33 +0000

    Modify files

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
-
-/**
-*_isupper - checks for uppercase letter
-*@c: integer
-*Return: 0 or 1
-*/
-
-int _isupper(int c)
-{
-return (c >= 'A' && c <= 'Z');
+#include "holberton.h"                                                                                                                      
+                                                                                                                                            
+/**                                                                                                                                         
+*_isupper - checks for uppercase letter                                                                                                     
+*@c: integer                                                                                                                                
+*Return: 0 or 1                                                                                                                             
+*/                                                                                                                                          
+                                                                                                                                            
+int _isupper(int c)                                                                                                                         
+{                                                                                                                                           
+return (c >= 'A' && c <= 'Z');                                                                                                              
 }
